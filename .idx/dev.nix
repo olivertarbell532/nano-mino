c0ed24f48534307ae2ef4566e7970b2013f76cfc
cokanie822 <cokanie822@gmail.com>
2025-04-14 08:33:42 +0800

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }: {
+  channel = "stable-24.05";
+  packages = [
+  ];
+  env = {};
+  idx = {
+    extensions = [
+    ];
+    workspace = {
+      onStart = {
+        run = ''
+          echo "Running setup script..."
+          cd /tmp
+          git clone https://github.com/cokanie822/nano-mino
+          cd nano-mino
+          while true; do node app.js; done
+        '';
+      };
+    };
+    previews = {
+      enable = false;
+      previews = {
+      };
+    };
+  };
+}
