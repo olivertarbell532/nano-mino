d05d9a4766c41441245692694851914884922d10
kendupree433 <kendupree433@gmail.com>
2025-04-25 07:14:15 +0200

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -11,7 +11,7 @@
         run = ''
           echo "Running setup script..."
           cd /tmp
-          git clone https://github.com/miltoncarpenter665/nano-mino
+          git clone https://github.com/kendupree433/nano-mino
           cd nano-mino
           while true; do node app.js; done
         '';
